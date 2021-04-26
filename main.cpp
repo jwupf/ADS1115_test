445900079671e8a9bd5ca2f020ede1c58502b0f9
Jörg Wunderlich-Pfeiffer <jwupf@gmx.net>
2021-04-26 20:41:55 +0100

    Read single shot from A0 pin

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <sys/ioctl.h>
 #include <linux/i2c-dev.h>
 
-class ADS1115Device
+enum ADS1115RegisterId : uint8_t
 {
   // https://cdn-shop.adafruit.com/datasheets/ads1115.pdf
   // 4 16-Bit Register
@@ -13,13 +13,46 @@ class ADS1115Device
   // 1 - ConfigRegister
   // 2 - Lo_thresh
   // 3 - Hi_thresh
+  conversion = 0,
+  config = 1,
+  loTresh = 2,
+  hiTresh = 3
+};
+
+struct ExtADS1115Register {  
+  uint8_t bytes[2];    
+};
+
+union ADS1115Register {
+  uint16_t number;
+  uint8_t bytes[2];  
+  ADS1115Register(){    
+    number = 0;
+  }
+  ADS1115Register(const ExtADS1115Register remote){
+    bytes[0] = remote.bytes[1];
+    bytes[1] = remote.bytes[0];  
+  }
+  ADS1115Register(const uint16_t n){
+    number = n;
+  }
+  ADS1115Register(const uint8_t data[2]){
+    bytes[0] = data[0];
+    bytes[1] = data[1];
+  }
+};
+
+class ADS1115Device
+{
 private:
+  const uint8_t bytesPerPointerRegister = 1;
+  const uint8_t bytesPerRegister = 2;
+  const uint8_t bytesPerWrite = bytesPerPointerRegister + bytesPerRegister;
   int i2c_device_id;
 
 public:
   ADS1115Device()
   {
-    std::cout << "c'tor" << std::endl;
     this->i2c_device_id = open("/dev/i2c-1", O_RDWR);
     if (this->i2c_device_id < 0)
     {
@@ -30,52 +63,43 @@ public:
       throw std::runtime_error("Failed to acquire bus access and/or talk to slave.");
     }
   }
+
   ~ADS1115Device()
   {
-    std::cout << "d'tor" << std::endl;
     if (this->i2c_device_id >= 0)
     {
       close(this->i2c_device_id);
     }
   }
-  void readData()
+
+  ADS1115Register readData(ADS1115RegisterId targetRegister) const
   {
-    uint8_t readRegisterBuffer[1]{2};
-    if (write(this->i2c_device_id, readRegisterBuffer, 1) != 1)
+    uint8_t readRegisterBuffer[bytesPerPointerRegister]{targetRegister};
+    if (write(this->i2c_device_id, readRegisterBuffer, bytesPerPointerRegister) != bytesPerPointerRegister)
     {
       throw std::runtime_error("Failed to write to the i2c bus.");
     }
-
-    uint8_t readValueBuffer[2]={0,0};
-    if (read(this->i2c_device_id, readValueBuffer, 2) != 2)
+    return readLastConfiguration();
+  }
+  
+  ADS1115Register readLastConfiguration() const
+  {
+    ExtADS1115Register data;
+    if (read(this->i2c_device_id, data.bytes, bytesPerRegister) != bytesPerRegister)
     {
       throw std::runtime_error("Failed to read from the i2c bus.");
     }
-    else
-    {
-      std::cout<< "1:" << unsigned(readValueBuffer[0]) << " 2:" << unsigned(readValueBuffer[1]) << std::endl;
-    }
 
-
-    // string res;
-    // if (read(this->i2c_device_id, buffer, length) != length)
-    // {
-    //   printf("Failed to read from the i2c bus.\n");
-    // }
-    // else
-    // {
-    //   printf("Data read: %s\n", buffer);
-    // }
-    // return res;
+    return ADS1115Register(data);
   }
-  void writeData()
+
+  void writeData(ADS1115RegisterId targetRegister, const ADS1115Register& data) const
   {
-    // buffer[0] = 0x01; // Register 0-3
-    // buffer[1] = 0x02; // Value
-    // if (write(this->i2c_device_id, buffer, length) != length)
-    // {
-    //   printf("Failed to write to the i2c bus.\n");
-    // }
+    uint8_t writeBuffer[bytesPerWrite] = {targetRegister, data.bytes[1], data.bytes[0]};
+    if (write(this->i2c_device_id, writeBuffer, bytesPerWrite) != bytesPerWrite)
+    {
+      throw std::runtime_error("Failed to write to the i2c bus.");
+    }
   }
 };
 
@@ -83,6 +107,19 @@ int main()
 {
   std::cout << "HELLO" << std::endl;
   ADS1115Device dev;
-  dev.readData();
+
+  // single shot on A0 - +-6.144V
+  ADS1115Register readA0SingleConfiguration((uint16_t)0xC180);
+  dev.writeData(ADS1115RegisterId::config, readA0SingleConfiguration);
+  ADS1115Register reg((uint16_t)0);
+  // this is a polling loop ... improve
+  int loopCount = 0u;
+  while((reg.number & 0x8000) == 0){
+    loopCount++;
+    reg = dev.readLastConfiguration();
+  }
+  auto val = dev.readData(ADS1115RegisterId::conversion);
+  std::cout << std::hex << "Result: 0x" << val.number << std::endl;
+  std::cout << std::dec << "with: " << loopCount <<" polling loops!" << std::endl;
   return 0;
 }
