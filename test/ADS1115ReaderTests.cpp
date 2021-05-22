efd24a5b87361117d066a7fe6ade77111cf8414b
Jörg Wunderlich-Pfeiffer <jwupf@gmx.net>
2021-05-22 20:39:27 +0100

    Rename tests files

--- test/ADS1115ReaderTests.cpp
+++ test/ADS1115ReaderTests.cpp
@@ -1,9 +1,12 @@
 #include <gtest/gtest.h>
 
 // Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
+TEST(HelloTest, BasicAssertion1) {
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
+}
+
+TEST(HelloTest, BasicAssertion2) {
   // Expect equality.
   EXPECT_EQ(7 * 6, 42);
 }
